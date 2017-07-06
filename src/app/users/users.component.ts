0e09b19f0e84c189cf462fbe083e93241e08b4c2
howard <ntut412@gmail.com>
2017-07-06 14:25:36 +0800

    2版

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,14 @@
 import { Component ,OnInit} from '@angular/core';
 import { Http, RequestOptions ,Headers } from '@angular/http';
 import { Observable } from 'rxjs';
+import { HttpService } from './http.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers: [HttpService]
 })
 export class UsersComponent implements OnInit {
   inputHint = 'What needs to be done?';  //輸入框預設字串
@@ -14,55 +17,56 @@ export class UsersComponent implements OnInit {
   filterType = 'All';
   toggleAll = false;
 
-  private requestOptions = new RequestOptions({
-    headers: new Headers({
-      'authorization': 'token e29f6eea-47d3-4c2e-88ae-ee759e777a64'
-    })
-  });
+  // private requestOptions = new RequestOptions({
+  //   headers: new Headers({
+  //     'authorization': 'token e29f6eea-47d3-4c2e-88ae-ee759e777a64'
+  //   })
+  // });
 
-  private dataUrl = 'https://jsonbin.org/jiaming0708/howard';
-  constructor (private http: Http){
+  // private dataUrl = 'https://jsonbin.org/jiaming0708/howard';
+  constructor (private httpservice: HttpService){
   }
 
   ngOnInit(){      //載入畫面先執行
-    this.getTodes().subscribe(data =>{
+    this.httpservice.getTodes().subscribe(data =>{
       this.todos = data;
       console.log("555",data);
     });
   }
 
-  getTodes(){
-    return this.http.get( this.dataUrl, this.requestOptions).map(res => {
-      console.log("res",res);
-      return res.json();
-    }).catch(error =>{
+  // getTodes(){
+  //   return this.http.get( this.dataUrl, this.requestOptions).map(res => {
+  //     console.log("res",res);
+  //     return res.json();
+  //   }).catch(error =>{
 
-      console.log(error);
-      return Observable.of<any[]>([]);
+  //     console.log(error);
+  //     return Observable.of<any[]>([]);
 
-    })
-  }
+  //   })
+  // }
 
-  saveTodos (newTodos: any[]){
-    return this.http.post(this.dataUrl,newTodos ,this.requestOptions).map(res => {
-      this.todos = res.json();
-    }).catch(error =>{
-      console.log(error);
-      return Observable.of<any[]>([]);
-    })
-  }
+  // saveTodos (newTodos: any[]){
+  //   return this.http.post(this.dataUrl,newTodos ,this.requestOptions).map(res => {
+  //     this.todos = res.json();
+  //   }).catch(error =>{
+  //     console.log(error);
+  //     return Observable.of<any[]>([]);
+  //   })
+  // }
 
 
 
   addTodo() {
-
+    console.log("aaa");
     let newTodos = [...this.todos];
     newTodos.push({
       text:  this.todo,
       done : false,    //true 代表刪除
     });
-    this.saveTodos(newTodos).subscribe(data => {
+    this.httpservice.saveTodos(newTodos).subscribe(data => {
       this.todo = '';
+      this.todos = data;
     });
 
     // this.todos.push({
@@ -77,7 +81,7 @@ export class UsersComponent implements OnInit {
     // this.todos = this.todos.filter(item => { return !item.done; });
     // console.log("123");
     let newTodos = this.todos.filter(item => { return !item.done; });
-    this.saveTodos(newTodos).subscribe(data => { });
+    this.httpservice.saveTodos(newTodos).subscribe(data => { this.todos = data;});
   }
   filterTypeChanged(filterType: string) {
     this.filterType = filterType;
@@ -93,17 +97,20 @@ export class UsersComponent implements OnInit {
     newTodos.forEach(item => {
        item.done = value
      });
-     this.saveTodos(newTodos).subscribe(data => { });
+     this.httpservice.saveTodos(newTodos).subscribe(data => { this.todos = data; });
   }
   updateToggleAllState(){
+    let newTodos = [...this.todos];
     console.log("22");
     this.toggleAll = this.todos.filter(item => { return !item.done; }).length === 0;
+    this.httpservice.saveTodos(newTodos).subscribe(data => { this.todos = data; });
   }
   removeTodo(todo){
+    console.log("123");
     let newTodos = [...this.todos];
     // this.todos.splice(this.todos.indexOf(todo),1);
     newTodos.splice(this.todos.indexOf(todo),1);
-    this.saveTodos(newTodos).subscribe(data => { });
+    this.httpservice.saveTodos(newTodos).subscribe(data => { this.todos = data;});
   }
 
 }
